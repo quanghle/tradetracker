abd83af7f2da7e40afa6f0d4cd8ad742a6eacd29
Quang Le <q.le@outlook.com>
2020-12-26 12:30:44 -0500

    update script

--- dev/cleandb.sql
+++
@@ -1,6 +0,0 @@
-SELECT 'CREATE DATABASE test'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'test')\gexec
-\c test
-DROP DATABASE postgres WITH (FORCE);
-CREATE DATABASE postgres;
-\c postgres
---
+++ scripts/debug-mode.sh
@@ -0,0 +1,1 @@
+docker-compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.debug.yml up $1
